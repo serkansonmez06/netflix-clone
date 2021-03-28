ae263321880f72f84deceaa92bbf81cfa71824b9
Serkan <srknsnmz83@gmail.com>
2021-03-28 17:53:09 -0400

    firebase API key removoed

--- clone-netflix/src/firebase.js
+++ clone-netflix/src/firebase.js
@@ -2,13 +2,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCeofoS-cOz8IdJzEQC3gihtqPj2JPYFCw",
-    authDomain: "netflix-clone-c0da9.firebaseapp.com",
-    projectId: "netflix-clone-c0da9",
-    storageBucket: "netflix-clone-c0da9.appspot.com",
-    messagingSenderId: "450272725194",
-    appId: "1:450272725194:web:29a84d36432c5cdacd8c33",
-    measurementId: "G-0CRNBMNMDK"
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
